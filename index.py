e0bc49849514012c43821c517256b70e801c1dcf
张长春 <zhangchangchun@dodoca.com>
2018-04-18 20:35:19 +0800

    test

--- index.py
+++ index.py
@@ -8,6 +8,10 @@ manage = Manager(app)
 def index():
     return render_template('index.html')
 
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html',name=name)
+
 if __name__ == "__main__":
     #manage.run()
     app.run(debug=True,host='0.0.0.0')
