4f000ba378ba38a07c7edb37270dcefcfcb9a239
张长春 <zhangchangchun@dodoca.com>
2018-04-18 18:22:17 +0800

    模板引擎

---
+++ index.py
@@ -0,0 +1,12 @@
+from flask import Flask,request,render_template
+from flask_script import Manager
+
+app = Flask(__name__)
+manage = Manager(app)
+
+@app.route('/')
+def index():
+    return render_template('tpl/index.html')
+
+if __name__ == "__main__":
+    manage.run()
