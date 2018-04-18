e45ddf837b2c132936aaaf4ecb64747206db5f23
张长春 <zhangchangchun@dodoca.com>
2018-04-18 17:03:22 +0800

    添加hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+from flask import Flask,request
+from flask_script import Manger
+
+app = Flask(__name__)
+manager = Manger(app)
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Hello World ! </h1><h2>%s</h2>' % user_agent
+
+@app.route('/user/<name>')
+def user(name):
+        return "<h1>Hello %s" % name
+
+if __name__=="__name__":
+    app.run(debug=True,host='0.0.0.0')
